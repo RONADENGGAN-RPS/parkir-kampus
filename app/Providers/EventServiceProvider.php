fa43d285be58c719e79fd72d4b4ae89686e10955
RONADENGGAN-RPS <simbolonrobin94@gmail.com>
2026-05-06 10:03:26 +0700

    Fix provider class declarations

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -11,16 +11,8 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event to listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
     protected $listen = [];
 
-    /**
-     * Register any events for your application.
-     */
     public function boot(): void
     {
         parent::boot();
