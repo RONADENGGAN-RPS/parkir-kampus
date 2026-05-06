8ea3636ae3c27b2689b8f0b7daf80fc3b23ff189
RONADENGGAN-RPS <simbolonrobin94@gmail.com>
2026-05-06 09:45:37 +0700

    Document event listener mappings in EventServiceProvider
    Added documentation for event listener mappings and boot method.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -11,8 +11,16 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
+    /**
+     * The event to listener mappings for the application.
+     *
+     * @var array<class-string, array<int, class-string>>
+     */
     protected $listen = [];
 
+    /**
+     * Register any events for your application.
+     */
     public function boot(): void
     {
         parent::boot();
